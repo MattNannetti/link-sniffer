28b4285a49efb837085bb2547436c5e2b5d9edcd
Matt Nannetti <mattnannetti@gmail.com>
2020-12-18 14:25:16 +0100

    update routes

--- linkSniffer/routes.py
+++ linkSniffer/routes.py
@@ -1,7 +1,7 @@
 import os
 from flask import render_template, url_for, flash, redirect
 from linkSniffer import app
-
+from bs4 import BeautifulSoup
 
 #URL = 'http://127.0.0.1:2000/api/v1/ressources/cocktails/all'
 #price = str(requests.request("GET", url, headers=headers, params = querystring).json()['Quotes'][0]['MinPrice'])
