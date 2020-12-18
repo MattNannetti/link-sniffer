cd4488d5161ba8d1933c0ce289d16aef3cf28600
Maximilien <pierremaximilien38@gmail.com>
2020-12-18 14:23:16 +0100

    add algo.py with some email scraping

--- linkSniffer/routes.py
+++ linkSniffer/routes.py
@@ -1,12 +1,28 @@
 import os
 from flask import render_template, url_for, flash, redirect
+from linkSniffer.forms import SearchForm
 from linkSniffer import app
+from bs4 import BeautifulSoup
+import requests
+import re
 
 
 #URL = 'http://127.0.0.1:2000/api/v1/ressources/cocktails/all'
-#price = str(requests.request("GET", url, headers=headers, params = querystring).json()['Quotes'][0]['MinPrice'])
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    form = SearchForm()
+
+    url = request.args.get('imput_url')
+
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    soup.find(re.compile('[\w\.-]+@[\w\.-]+\.\w+'))
+
+
+
+
+    return render_template('index.html', form = form)
+
